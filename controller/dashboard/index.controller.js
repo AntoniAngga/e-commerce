2ccc4b74c31941f404ea0ce93e78d7d616a168cf
Antoni Angga <antoniangga14@gmail.com>
2020-08-30 20:00:58 +0700

    add redirect

--- controller/dashboard/index.controller.js
+++ controller/dashboard/index.controller.js
@@ -1,15 +1,3 @@
-const { PPOB_history_transaction } = require('../../models/index');
-
-exports.index = async (_req, res) => {
-  try {
-    const history_transactions = await PPOB_history_transaction.findAll({
-      include: 'user',
-    });
-    res.render('dashboard/index.ejs', {
-      history_transactions: history_transactions,
-    });
-  } catch (err) {
-    res.status(err.status || 500);
-    res.render('error');
-  }
+exports.index = (_req, res) => {
+  res.redirect('/ppob-history-transaction');
 };
